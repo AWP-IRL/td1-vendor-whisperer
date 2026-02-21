af22be9f67ee007285c0ce260b8d778c416ef221
AWP-IRL <awp_irl@proton.me>
2026-02-20 21:20:57 -0500

    Update premium.js

--- docs/assets/js/premium.js
+++ docs/assets/js/premium.js
@@ -1,3 +1,4 @@
+/*
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,7 +27,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
+*/
 // Timestamp and countdown calculation
 function getTimeZoneDisplayName(zoneNameFormat = "short") {
 	let formatter = new Intl.DateTimeFormat(undefined, {
@@ -74,3 +75,4 @@ function countDownOut (toDate, toElement) {
 	, 1000);
 
 }
+
