ee13718096315fc6ade98a54bb2794f50a0903d3
AWP-IRL <awp_irl@proton.me>
2026-02-27 21:32:15 -0500

    Update level-34.js

--- docs/assets/js/level-34.js
+++ docs/assets/js/level-34.js
@@ -22,7 +22,7 @@ function getTimeZoneDisplayName(zoneNameFormat = "short") {
 		.find(part => part.type === "timeZoneName")['value'];
 }
 
-var dtmUTC = new Date("2/29/2026 0:00:00 UTC");
+var dtmUTC = new Date("3/7/2026 0:00:00 UTC");
 var msUTC = dtmUTC.getTime();
 var strCDLoc = '34count'
 countDownOut(msUTC, strCDLoc);
@@ -60,3 +60,4 @@ function countDownOut (toDate, toElement) {
 	, 1000);
 
 }
+
