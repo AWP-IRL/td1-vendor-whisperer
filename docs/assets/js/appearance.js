0b629114f017cd93ad1889088d123e5b52bc5b15
AWP-IRL <awp_irl@proton.me>
2026-03-04 19:28:53 -0500

    Update appearance.js

--- docs/assets/js/appearance.js
+++ docs/assets/js/appearance.js
@@ -36,7 +36,7 @@ function getTimeZoneDisplayName(zoneNameFormat = "short") {
 		.find(part => part.type === "timeZoneName")['value'];
 }
 
-var dtmUTC = new Date("3/5/2026 0:00:00 UTC");
+var dtmUTC = new Date("3/6/2026 0:00:00 UTC");
 var msUTC = dtmUTC.getTime();
 var strCDLoc = 'appcount'
 countDownOut(msUTC, strCDLoc);
