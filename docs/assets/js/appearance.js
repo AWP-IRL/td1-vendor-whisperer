69ff0b2661fdf4306921d721d2fb5aee9078529d
AWP-IRL <awp_irl@proton.me>
2026-03-07 11:49:59 -0500

    Update appearance.js

--- docs/assets/js/appearance.js
+++ docs/assets/js/appearance.js
@@ -39,7 +39,7 @@ function getTimeZoneDisplayName(zoneNameFormat = "short") {
 var strUTC = document.getElementById('appstamp').innerHTML;
 var dtmUTC = new Date(strUTC);
 var msUTC = dtmUTC.getTime();
-var strCDLoc = 'appcount'
+var strCDLoc = 'appcount';
 countDownOut(msUTC, strCDLoc);
 var dtmUTCOut = '' + dtmUTC.getUTCFullYear() + '-' + ('0' + (dtmUTC.getUTCMonth() + 1)).slice(-2) + '-' + ('0' + dtmUTC.getUTCDate()).slice(-2) + ' ' + dtmUTC.getUTCHours() + ':' + ('0' + dtmUTC.getUTCMinutes()).slice(-2) + ' UTC'
 var strUTZOut = '' + dtmUTC.getFullYear() + '-' + ('0' + (dtmUTC.getMonth() + 1)).slice(-2) + '-' + ('0' + dtmUTC.getDate()).slice(-2) + ' ' + dtmUTC.getHours() + ':' + ('0' + dtmUTC.getMinutes()).slice(-2) + ' UTC' + dtmUTC.getTimezoneOffset() / -60 + ' (' + getTimeZoneDisplayName('short') + ')';
