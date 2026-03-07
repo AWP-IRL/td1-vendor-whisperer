44f7052fd02df1639c95e77e7de8b5b9fdecfb33
AWP-IRL <awp_irl@proton.me>
2026-03-06 21:38:59 -0500

    Update blueprint.js

--- docs/assets/js/blueprint.js
+++ docs/assets/js/blueprint.js
@@ -22,7 +22,7 @@ function getTimeZoneDisplayName(zoneNameFormat = "short") {
 		.find(part => part.type === "timeZoneName")['value'];
 }
 
-var dtmUTC = new Date("3/7/2026 0:00:00 UTC");
+var dtmUTC = new Date("3/14/2026 0:00:00 UTC");
 var msUTC = dtmUTC.getTime();
 var strCDLoc = 'bpcount'
 countDownOut(msUTC, strCDLoc);
@@ -60,4 +60,5 @@ function countDownOut (toDate, toElement) {
 	, 1000);
 
 }
+
 
