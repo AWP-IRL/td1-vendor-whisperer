9af6704ab0d0c86d6474f856a453ea71634a5504
AWP-IRL <awp_irl@proton.me>
2026-03-29 08:59:14 -0400

    Update ge.js

--- docs/assets/js/ge.js
+++ docs/assets/js/ge.js
@@ -1,3 +1,4 @@
+// collapsible
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -14,3 +15,49 @@ for (i = 0; i < coll.length; i++) {
 
 }
 
+// function to get next GE start date, parameter is known previous GE start date
+function nextGEDate (dtmStart) {
+
+	//get next start date
+	var dtmStartNext = new Date(dtmStart);
+	dtmStartNext.setDate(dtmStart.getUTCDate() + 168);
+	var dtmEndNext = new Date(dtmStart);
+	dtmEndNext.setDate(dtmStart.getUTCDate() + 175);
+
+	//compare next end date to today and if today > end date, call recursive
+	var dtmNOW = new Date();
+	var dtmBOD = new Date(dtmNOW.setUTCHours(0,0,0,0));
+	if (dtmBOD > dtmEndNext) {
+		return nextGEDate(dtmStartNext);
+	} else {
+		return dtmStartNext;
+	};
+}
+
+// Process dates in all .ge-next
+var geNexts = document.querySelectorAll(".ge-next");
+console.log(geNexts);
+geNexts.forEach((element) => {
+	var strGEStart = element.childNodes[2].innerHTML;
+	var dtmGEStart = new Date(strGEStart);
+	var dtmGEEnd = new Date(dtmGEStart);
+	dtmGEEnd.setDate(dtmGEStart.getUTCDate() + 7);
+	var dtmNOW = new Date();
+	var dtmBOD = new Date(dtmNOW.setUTCHours(0,0,0,0));
+	if (dtmBOD > dtmGEEnd) {
+		var dtmGEStartNext = nextGEDate(dtmGEStart);
+		const strGEStartNext = dtmGEStartNext.toLocaleString('en-US', { month: 'short' }) + " " + dtmGEStartNext.getDate() + ", " + dtmGEStartNext.getFullYear();
+		var dtmGEEndNext = new Date(dtmGEStartNext);
+		dtmGEEndNext.setDate(dtmGEStartNext.getUTCDate() + 7);
+		const strGEEndNext = dtmGEEndNext.toLocaleString('en-US', { month: 'short' }) + " " + dtmGEEndNext.getDate() + ", " + dtmGEEndNext.getFullYear();
+		element.childNodes[2].innerHTML = strGEStartNext;
+		element.childNodes[4].innerHTML = strGEEndNext;
+		element.childNodes[6].innerHTML = "(projected)";
+	} else if (dtmBOD >= dtmGEStart && dtmBOD <= dtmGEEnd) {
+		element.childNodes[0].innerHTML = "Now: ";
+		element.attributes.class =  element.classList.add("ge-now");
+	} else {
+		//GE is in the future, nothing to do
+	};
+
+})
