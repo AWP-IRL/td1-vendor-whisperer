fc3924b5991aee27d731b249ccd2935fdb03a346
AWP-IRL <awp_irl@proton.me>
2026-05-19 09:01:14 -0400

    toggletip update

--- docs/assets/js/level-34.js
+++ docs/assets/js/level-34.js
@@ -61,3 +61,78 @@ function countDownOut (toDate, toElement) {
 	, 1000);
 
 }
+
+// TOGGLETIPS
+(function() {
+  // Get all the toggletip buttons
+  var toggletipTexts = document.querySelectorAll('[data-toggletip]');
+
+  // Iterate over them
+  Array.prototype.forEach.call(toggletipTexts, function(toggletipText) {
+    // Create the container element
+    var container = document.createElement('span');
+    container.setAttribute('class', 'toggletip-container');
+    
+    // Put it before the original element in the DOM
+    toggletipText.parentNode.insertBefore(container, toggletipText);
+    
+    // Create the button element
+    var toggletip = document.createElement('button');
+    toggletip.setAttribute('class', 'button-info');
+    toggletip.setAttribute('type', 'button');
+    toggletip.setAttribute('aria-label', 'more info');
+    toggletip.setAttribute('data-toggletip-content', toggletipText.textContent);
+    toggletip.textContent = 'i';
+    
+    // Place the button element in the container
+    container.appendChild(toggletip);
+    
+    // Create the live region
+    var liveRegion = document.createElement('span');
+    liveRegion.setAttribute('role', 'status');
+    
+    // Place the live region in the container
+    container.appendChild(liveRegion);
+    
+    // Remove the original element
+    toggletipText.parentNode.removeChild(toggletipText);
+    
+    // Build `data-tooltip-content` 
+    var message = toggletip.getAttribute('data-toggletip-content');
+    toggletip.setAttribute('data-toggletip-content', message);
+    toggletip.removeAttribute('title');
+    
+    // Get the message from the data-content element
+    var message = toggletip.getAttribute('data-toggletip-content').replace('%0D%0A', '<br>');
+	var message = message.replace('%0D%0A%0D%0A', '<br><br>');
+	
+    // Get the live region element
+    var liveRegion = toggletip.nextElementSibling;
+
+    // Toggle the message
+    toggletip.addEventListener('click', function() {
+        liveRegion.innerHTML = '';
+        window.setTimeout(function() {
+          liveRegion.innerHTML = '<div class="toggletip-bubble">'+ message +'</div>';
+        }, 100);
+    });
+
+    // Close on outside click
+    document.addEventListener('click', function (e) {
+      if (toggletip !== e.target) {
+        liveRegion.innerHTML = '';
+      }                        
+    });
+
+    // Remove toggletip on ESC
+    toggletip.addEventListener('keydown', function(e) {
+      if ((e.keyCode || e.which) === 27)
+      liveRegion.innerHTML = '';
+    });
+    
+    // Remove on blur
+    toggletip.addEventListener('blur', function (e) {
+      liveRegion.innerHTML = '';
+    });
+  });
+}());
